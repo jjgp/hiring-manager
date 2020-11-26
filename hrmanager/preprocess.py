ef3da6c717a45e8c42f9a37dc997cc1fcc1336cb
Jason Prasad <jasongprasad@gmail.com>
2020-11-25 22:06:11 -0500

    Generates criterion and feature columns

--- hrmanager/preprocess.py
+++ hrmanager/preprocess.py
@@ -1,10 +1,52 @@
 # %%
 import pandas as pd
 
-# %%
-df = pd.read_csv("../data/train.csv", index_col="UNIQUE_ID")
+# Criterion
+criterion_cols = [
+    "Overall_Rating",
+    "Technical_Skills",
+    "Teamwork",
+    "Customer_Service",
+    "Hire_Again",
+    "High_Performer",
+    "Retained",
+    "Protected_Group"
+]
+
+# Biodata/Work History Items
+biodata_cols = [f"Biodata_{num:02}" for num in range(1, 21)]
+
+# Personality/Work Style Items
+pscale_cols = [
+    f"PScale{first:02}_Q{second}"
+    for first in range(1, 14)
+    for second in range(1, 5)
+]
+pscale_cols.extend(["PScale06_Q5", "PScale06_Q6", "PScale13_Q5"])
+
+# Scenario Interpretation
+scenario_cols = [
+    f"Scenario{first}_{second}"
+    for first in ["1", "2"]
+    for second in ["1", "2", "3", "4", "5", "6", "7", "8", "Time"]
+]
+
+# Situational Judgment Items
+sj_cols = [
+    f"SJ_{first}_{second}"
+    for first in ["Least", "Most", "Time"]
+    for second in range(1, 10)
+]
+
+# Features
+feature_cols = biodata_cols \
+    + pscale_cols \
+    + scenario_cols \
+    + sj_cols
+
+use_cols = ["UNIQUE_ID"] + criterion_cols + feature_cols
 
 # %%
-df.head(1)
+df = pd.read_csv("../data/train.csv", index_col="UNIQUE_ID", usecols=use_cols)
 
 # %%
