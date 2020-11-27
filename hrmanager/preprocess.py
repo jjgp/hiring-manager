853385fe16a0f6be2e9f52c81240debb63129a8d
Jason Prasad <jasongprasad@gmail.com>
2020-11-26 23:49:48 -0500

    Adds feature importance logging
    https://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html

--- hrmanager/preprocess.py
+++ hrmanager/preprocess.py
@@ -1,6 +1,7 @@
 # %%
+import numpy as np
 import pandas as pd
-from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
@@ -19,8 +20,9 @@ numeric_features_df = predictors_df.loc[:, numeric_cols]
 X = OneHotEncoder().fit_transform(binary_features_df)
 Y = criterion_df["Retained"].to_numpy()
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
+
 # %%
-estimator = AdaBoostClassifier()
+estimator = ExtraTreesClassifier(n_estimators=250, random_state=0)
 estimator.fit(X_train, Y_train)
 Y_pred = estimator.predict(X_test)
 
@@ -29,4 +31,18 @@ confusion_matrix(Y_test, Y_pred)
 
 # %%
 accuracy_score(Y_test, Y_pred)
+
+# %%
+importances = estimator.feature_importances_
+std = np.std(
+    [tree.feature_importances_ for tree in estimator.estimators_],
+    axis=0
+)
+indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+for f in range(X.shape[1]):
+    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
 # %%
