591983235c8fa732eb874a0b7860813df8ea0d33
Jason Prasad <jasongprasad@gmail.com>
2020-11-28 12:54:18 -0500

    Fills na with 0 for binary and mode for numerical

--- hrmanager/preprocess.py
+++ hrmanager/preprocess.py
@@ -6,7 +6,8 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
-df = pd.read_csv("../data/train.csv", index_col="UNIQUE_ID")
+# %%
+df = pd.read_csv("../data/train.csv", index_col="UNIQUE_ID", na_values=" ")
 criterion_df = df.loc[:, "Overall_Rating":"Retained"]
 predictors_df = df.loc[:, "SJ_Most_1":"PScale13_Q5"]
 
@@ -14,15 +15,20 @@ predictors_df = df.loc[:, "SJ_Most_1":"PScale13_Q5"]
 numeric_cols = ["Scenario1_Time", "Scenario2_Time"] \
     + [f"SJ_Time_{number}" for number in range(1, 10)]
 binary_features_df = predictors_df.drop(numeric_cols, axis=1)
+binary_features_df.fillna(0, inplace=True)
 numeric_features_df = predictors_df.loc[:, numeric_cols]
+for col in numeric_cols:
+    mode = numeric_features_df[col].mode(dropna=True).iloc[-1]
+    numeric_features_df[col] = numeric_features_df[col].fillna(mode)
 
 # %%
-X = OneHotEncoder().fit_transform(binary_features_df)
+X = OneHotEncoder().fit_transform(binary_features_df).toarray()
+X = np.concatenate((X, numeric_features_df.to_numpy()), axis=1)
 Y = criterion_df["Retained"].to_numpy()
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
 
 # %%
-estimator = ExtraTreesClassifier(n_estimators=250, random_state=0)
+estimator = ExtraTreesClassifier(random_state=0)
 estimator.fit(X_train, Y_train)
 Y_pred = estimator.predict(X_test)
 
