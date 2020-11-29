a9e55d4eb8b3d2a47928b7f619cdf97e667c1240
Jason Prasad <jasongprasad@gmail.com>
2020-11-28 20:54:50 -0500

    Keeps things in same file for speed

--- hrmanager/hrmanager.py
+++ hrmanager/hrmanager.py
@@ -1,12 +1,44 @@
 # %%
-from featurize import featurize
-from preprocess import preprocess_csv
+from schema import BINARY_COLS, NUMERIC_COLS
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+import pandas as pd
 
+# %%
+def preprocess_csv(filepath):  # noqa: E302
+    df = pd.read_csv(filepath, index_col="UNIQUE_ID", na_values=" ")
+    # NOTE: filling NaN with 0.0s
+    df[list(BINARY_COLS)] = df[list(BINARY_COLS)].fillna(0)
+    for col in NUMERIC_COLS:
+        mode = df[col].mode(dropna=True).iloc[-1]
+        # NOTE: filling NaN with the mode of that column
+        df[col] = df[col].fillna(mode)
+    return df
+
+
+# %%
+def featurize(df):  # noqa: E302
+    binary_features_matrix = OneHotEncoder().fit_transform(
+        df[list(BINARY_COLS)]
+    )
+    numeric_features_df = df[list(NUMERIC_COLS)]
+    X = np.concatenate(
+        (binary_features_matrix.toarray(), numeric_features_df.to_numpy()),
+        axis=1
+    )
+    # NOTE: for now Y is just the "Retained" column!
+    Y = df["Retained"].to_numpy()
+    return X, Y
+
+
+# %%
 df = preprocess_csv("../data/train.csv")
+df.head()
+
+# %%
 X, Y = featurize(df)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
 
