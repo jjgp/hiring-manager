6b48f3560c00ed98d1fc184dedf9a4b3d950e1d9
Jason Prasad <jasongprasad@gmail.com>
2020-12-03 00:45:00 -0500

    Multilabel classification

--- hrmanager/hrmanager.py
+++ hrmanager/hrmanager.py
@@ -4,29 +4,37 @@ from schema import INDEX_COL, PREDICTOR_COLS, PROTECTED_GROUP_COL, RETAINED_COL
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
 
 # %%
-usecols = [INDEX_COL, PROTECTED_GROUP_COL, RETAINED_COL] + list(PREDICTOR_COLS)
+predictor_cols = list(PREDICTOR_COLS)
+target_cols = [PROTECTED_GROUP_COL, RETAINED_COL]
+usecols = [INDEX_COL] + target_cols + predictor_cols
 train = pd.read_csv(
     "../data/train.csv",
     index_col="UNIQUE_ID",
     usecols=usecols,
     na_values=" "
 )
+train.dropna(subset=target_cols, inplace=True)
 train.head()
 
 # %%
-X = train[list(PREDICTOR_COLS)]
-y = train[RETAINED_COL]
+X = train[predictor_cols]
+y = train[target_cols]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=0)
 
 # %%
+estimator = XGBClassifier(n_estimators=1000, learning_rate=0.05, n_jobs=4)
+model = OneVsRestClassifier(estimator)
+
+# %%
 pipeline = Pipeline(steps=[
     ('preprocessor', SimpleImputer()),
-    ('model', XGBClassifier(n_estimators=1000, learning_rate=0.05, n_jobs=4))
+    ('model', model)
 ])
 pipeline.fit(X_train, y_train)
 y_pred = pipeline.predict(X_test)
