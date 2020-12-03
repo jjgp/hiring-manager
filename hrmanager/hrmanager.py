37011e5660e70a8b57db1fdbf09da240e987307f
Jason Prasad <jasonprasad@stockx.com>
2020-12-03 14:11:59 -0500

    Calculates scoring

--- hrmanager/hrmanager.py
+++ hrmanager/hrmanager.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from schema import HIGH_PERFORMER_COL, INDEX_COL, PREDICTOR_COLS, \
     PROTECTED_GROUP_COL, RETAINED_COL, TARGET_COLS
-from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -11,49 +10,23 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
 
-
-class HiringScorer(BaseEstimator, TransformerMixin):
-    def __init__(self, ordered_target_cols):
-        target_cols = list(TARGET_COLS)
-        if len(ordered_target_cols) != len(target_cols):
-            raise ValueError("Number of cols specified does not match")
-        cols_diff = set(ordered_target_cols) - set(target_cols)
-        if cols_diff:
-            raise ValueError(f"Unsupported cols preset: {cols_diff}")
-        self._target_cols = target_cols
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        """
-        Final_score = Overall_accuracy – Unfairness
-
-        Overall_accuracy=
-        Percentage_of_true_top_performers_hired * 25+
-        Percentage_of_true_retained_hired * 25 +
-        Percentage_of_true_retained_top_perf_hired * 50
-
-        Unfairness = Absolute_value(1 - Adverse_impact_ratio) * 100
-        """
-        return X
-
-
-hs = HiringScorer(list(TARGET_COLS))
-hs.transform(np.array([[0.6, 0.4, 0.1]]))
-
 # %%
 predictor_cols = list(PREDICTOR_COLS)
 target_cols = list(TARGET_COLS)
 usecols = [INDEX_COL] + target_cols + predictor_cols
-train = pd.read_csv("../data/train.csv", index_col="UNIQUE_ID",
-                    usecols=usecols, na_values=" ")
+train = pd.read_csv(
+    "../data/train.csv",
+    index_col="UNIQUE_ID",
+    usecols=usecols,
+    na_values=" "
+)
 train.dropna(subset=target_cols, inplace=True)
 
 X = train[predictor_cols]
 y = train[target_cols]
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.1, random_state=0)
+    X, y, test_size=0.1, random_state=0
+)
 
 # %%
 estimator = XGBClassifier(n_estimators=1000, learning_rate=0.05, n_jobs=4)
@@ -62,15 +35,72 @@ model = OneVsRestClassifier(estimator)
 pipeline = Pipeline(steps=[
     ('imputer', SimpleImputer()),
     ('model', model),
-    ('scorer', HiringScorer(target_cols))
 ])
 pipeline.fit(X_train, y_train)
-y_pred = pipeline.predict_proba(X_test)
+y_proba = pipeline.predict_proba(X_test)
 
 # %%
-y_thr = y_pred > 0.5
-print(accuracy_score(y_test[[HIGH_PERFORMER_COL]], y_thr[:, 0]))
-print(accuracy_score(y_test[[PROTECTED_GROUP_COL]], y_thr[:, 1]))
-print(accuracy_score(y_test[[RETAINED_COL]], y_thr[:, 2]))
+y_pred = y_proba > 0.5
+print(accuracy_score(y_test[[HIGH_PERFORMER_COL]], y_pred[:, 0]))
+print(accuracy_score(y_test[[PROTECTED_GROUP_COL]], y_pred[:, 1]))
+print(accuracy_score(y_test[[RETAINED_COL]], y_pred[:, 2]))
+
+# %%
+"""
+Final_score = Overall_accuracy – Unfairness
+
+Overall_accuracy=
+Percentage_of_true_top_performers_hired * 25+
+Percentage_of_true_retained_hired * 25 +
+Percentage_of_true_retained_top_perf_hired * 50
+
+Unfairness = Absolute_value(1 - Adverse_impact_ratio) * 100
+"""
+
+# The following is a scoring mechanism inspired by the above
+# It is a heursitic and not the actual calculation
+# The purpose is to augment the probabilities with a heuristic and sort them
+assert(target_cols == list(TARGET_COLS))
+hr_scores = np.zeros((y_proba.shape[0]))
+hr_scores = hr_scores[:] + 0.25 * y_proba[:, 0] + 0.25 * y_proba[:, 2] \
+    + 0.5 * y_proba[:, 0] * y_proba[:, 2]
+hr_scores = hr_scores[:] - np.abs(y_proba[:, 1] - 0.5)
+
+hr_score_col = "HR_SCORE"
+y_hired = y_test.copy()
+y_hired[hr_score_col] = hr_scores
+y_hired.sort_values(by=[hr_score_col], ascending=False, inplace=True)
+y_hired = y_hired.head(y_hired.shape[0] // 2)
+
+# %%
+thp_count_test = y_test[y_test[HIGH_PERFORMER_COL] == 1.0].shape[0]
+tr_count_test = y_test[y_test[RETAINED_COL] == 1.0].shape[0]
+thpr_count_test = y_test[
+    (y_test[HIGH_PERFORMER_COL] == 1.0) & (y_test[RETAINED_COL] == 1.0)
+].shape[0]
+protected_count_test = y_test[y_test[PROTECTED_GROUP_COL] == 1.0].shape[0]
+non_protected_count_test = y_test[y_test[PROTECTED_GROUP_COL] == 0.0].shape[0]
+
+thp_count_hired = y_hired[y_hired[HIGH_PERFORMER_COL] == 1.0].shape[0]
+tr_count_hired = y_hired[y_hired[RETAINED_COL] == 1.0].shape[0]
+thpr_count_hired = y_hired[
+    (y_hired[HIGH_PERFORMER_COL] == 1.0) & (y_test[RETAINED_COL] == 1.0)
+].shape[0]
+protected_count_hired = y_hired[y_hired[PROTECTED_GROUP_COL] == 1.0].shape[0]
+non_protected_count_hired = y_hired[y_hired[PROTECTED_GROUP_COL]
+                                    == 0.0].shape[0]
+
+adverse_impact_ratio = (protected_count_hired / protected_count_test) / \
+    (non_protected_count_hired / non_protected_count_test)
+unfairness = abs(1.0 - adverse_impact_ratio) * 100
+
+print(unfairness)
+
+print(
+    25 * (thp_count_hired / thp_count_test) +
+    25 * (tr_count_hired / tr_count_test) +
+    50 * (thpr_count_hired / thpr_count_test) -
+    unfairness
+)
 
 # %%
