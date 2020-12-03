055ce6d0ea48efe82e6cd02a2d205cdb1b5406bd
Jason Prasad <jasongprasad@gmail.com>
2020-12-03 00:02:36 -0500

    Uses XGBoost to predict retained

--- hrmanager/hrmanager.py
+++ hrmanager/hrmanager.py
@@ -1,35 +1,35 @@
 # %%
-from featurize import featurize
-from preprocess import preprocess_csv
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+import pandas as pd
+from schema import INDEX_COL, PREDICTOR_COLS, PROTECTED_GROUP_COL, RETAINED_COL
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-import numpy as np
-
-df = preprocess_csv("../data/train.csv")
-X, Y = featurize(df)
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1)
-
-estimator = ExtraTreesClassifier(random_state=0)
-estimator.fit(X_train, Y_train)
-Y_pred = estimator.predict(X_test)
+from sklearn.pipeline import Pipeline
+from xgboost import XGBClassifier
 
 # %%
-confusion_matrix(Y_test, Y_pred)
-
-# %%
-accuracy_score(Y_test, Y_pred)
-
-# %%
-importances = estimator.feature_importances_
-std = np.std(
-    [tree.feature_importances_ for tree in estimator.estimators_],
-    axis=0
+usecols = [INDEX_COL, PROTECTED_GROUP_COL, RETAINED_COL] + list(PREDICTOR_COLS)
+train = pd.read_csv(
+    "../data/train.csv",
+    index_col="UNIQUE_ID",
+    usecols=usecols,
+    na_values=" "
 )
-indices = np.argsort(importances)[::-1]
-
-print("Feature ranking:")
-for f in range(X.shape[1]):
-    print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+train.head()
 
 # %%
+X = train[list(PREDICTOR_COLS)]
+y = train[RETAINED_COL]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=0)
+
+# %%
+pipeline = Pipeline(steps=[
+    ('preprocessor', SimpleImputer()),
+    ('model', XGBClassifier())
+])
+pipeline.fit(X_train, y_train)
+y_pred = pipeline.predict(X_test)
+
+# %%
+accuracy_score(y_test, y_pred)
