fe88c793b37e5af159762ede60acbcc86f57f5fd
Jason Prasad <jasongprasad@gmail.com>
2021-01-12 17:45:38 -0500

    Removing gitpod and attempting to use codespaces

--- hrmanager/hrmanager.py
+++ hrmanager/hrmanager.py
@@ -1,14 +1,20 @@
 # %%
 import numpy as np
 import pandas as pd
-from schema import (HIGH_PERFORMER_COL, INDEX_COL, PREDICTOR_COLS,
-                    PROTECTED_GROUP_COL, RETAINED_COL, TARGET_COLS)
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
+
+from hrmanager.schema import HIGH_PERFORMER_COL
+from hrmanager.schema import INDEX_COL
+from hrmanager.schema import PREDICTOR_COLS
+from hrmanager.schema import PROTECTED_GROUP_COL
+from hrmanager.schema import RETAINED_COL
+from hrmanager.schema import TARGET_COLS
 
 # %%
 predictor_cols = list(PREDICTOR_COLS)
@@ -18,7 +24,7 @@ train = pd.read_csv(
     "../data/train.csv",
     index_col="UNIQUE_ID",
     usecols=usecols,
-    na_values=" "
+    na_values=" ",
 )
 train.dropna(subset=target_cols, inplace=True)
 
@@ -30,7 +36,7 @@ y = train[target_cols]
 high_performer_retained_col = f"{HIGH_PERFORMER_COL}_{RETAINED_COL}"
 y[high_performer_retained_col] = y.apply(
     lambda row: int(row[HIGH_PERFORMER_COL] > 0 and row[RETAINED_COL] > 0),
-    axis=1
+    axis=1,
 )
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
@@ -39,10 +45,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 estimator = XGBClassifier(n_estimators=1000, learning_rate=0.05, n_jobs=4)
 model = OneVsRestClassifier(estimator)
 
-pipeline = Pipeline(steps=[
-    ('imputer', SimpleImputer()),
-    ('model', model),
-])
+pipeline = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer()),
+        ("model", model),
+    ],
+)
 pipeline.fit(X_train, y_train)
 y_proba = pipeline.predict_proba(X_test)
 
@@ -76,10 +84,11 @@ Unfairness = Absolute_value(1 - Adverse_impact_ratio) * 100
 # The following is a scoring mechanism inspired by the above
 # It is a heursitic and not the actual calculation
 # The purpose is to augment the probabilities with a heuristic and sort them
-assert(target_cols == list(TARGET_COLS))
+assert target_cols == list(TARGET_COLS)
 hr_scores = np.zeros((y_proba.shape[0]))
-hr_scores = hr_scores[:] + 0.25 * y_proba[:, 0] + 0.25 * y_proba[:, 2] \
-    + 0.5 * y_proba[:, 3]
+hr_scores = (
+    hr_scores[:] + 0.25 * y_proba[:, 0] + 0.25 * y_proba[:, 2] + 0.5 * y_proba[:, 3]
+)
 hr_scores = hr_scores[:] + 0.1 * y_proba[:, 1]
 
 hr_score_col = "HR_SCORE"
@@ -102,20 +111,20 @@ thpr_count_hired = y_hired[
     (y_hired[HIGH_PERFORMER_COL] == 1.0) & (y_test[RETAINED_COL] == 1.0)
 ].shape[0]
 protected_count_hired = y_hired[y_hired[PROTECTED_GROUP_COL] == 1.0].shape[0]
-non_protected_count_hired = y_hired[y_hired[PROTECTED_GROUP_COL]
-                                    == 0.0].shape[0]
+non_protected_count_hired = y_hired[y_hired[PROTECTED_GROUP_COL] == 0.0].shape[0]
 
-adverse_impact_ratio = (protected_count_hired / protected_count_test) / \
-    (non_protected_count_hired / non_protected_count_test)
+adverse_impact_ratio = (protected_count_hired / protected_count_test) / (
+    non_protected_count_hired / non_protected_count_test
+)
 unfairness = abs(1.0 - adverse_impact_ratio) * 100
 
 print(unfairness)
 
 print(
-    25 * (thp_count_hired / thp_count_test) +
-    25 * (tr_count_hired / tr_count_test) +
-    50 * (thpr_count_hired / thpr_count_test) -
-    unfairness
+    25 * (thp_count_hired / thp_count_test)
+    + 25 * (tr_count_hired / tr_count_test)
+    + 50 * (thpr_count_hired / thpr_count_test)
+    - unfairness,
 )
 
 # %%
